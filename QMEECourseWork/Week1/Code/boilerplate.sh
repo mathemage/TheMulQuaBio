c1a8b6db2094895567066000a1412e26bf229bbd
Karel Ha <mathemage@gmail.com>
2019-10-01 15:20:42 +0100

    Create boilerplate.sh
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ QMEECourseWork/Week1/Code/boilerplate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Author: Karel Ha mathemage@gmail.com
+# Script: boilerplate.sh
+# Desc: simple boilerplate for shell scripts
+# Arguments: none
+# Date: Oct 2018
+
+echo -e "\nThis is a shell script! \n" #what does -e do?
+
+#exit
